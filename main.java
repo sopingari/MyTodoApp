9408512ec038a8a0d5630b3980646de505e4edc1
sopingari <180606189+sopingar@users.noreply.github.com>
2024-10-01 18:19:56 +0000

    Modification from add-todo branch

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 import java.util.Date;
 
 public class TodoList {
+    //Modification from the add-todo branch
     // Class to represent a single Todo item with a description and a due date
     class TodoItem {
         String description;
